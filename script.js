8809b8b4ec142a72bdc2b0d186b4b7b07c20ab81
eucarlasimoni <161639492+eucarlasimoni@users.noreply.github.com>
2025-03-18 16:02:34 -0300

    18-03

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 document:addEventListener('DOMContentLoaded'function(){
 const aumentaFonteBotao=document.getElementById('aumentar-fonte');
-let tamanhoAtualFonte=1;
+const diminuirFonteBotao=document.getElementById('diminuir-fonte');
 
+let tamanhoAtualFonte=1;
 aumentaFonteBotao.addEventListener('click',function(){
     tamanhoAtual +=0.1;
     document.body.style.fontSize=${tamanhoAtualFonte}rem
-
-})
-
-})
+    })
 diminuirFonteBotao.addEventListener{'click',function() {
-    tamanhoAtual -=0.1;
-    document.body.style.fontSize=${tamanhoAtualFonte}rem
-  
+tamanhoAtual -=0.1;
+document.body.style.fontSize=${tamanhoAtualFonte}rem
 })
-
+ 
 })
